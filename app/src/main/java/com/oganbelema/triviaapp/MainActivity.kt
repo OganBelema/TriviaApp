8d64c52a9bf2d56999fb46532c4d0bddd411972c
Oganbelema <oganbelema@gmail.com>
2022-08-22 14:02:49 +0100

    tested question fetching implementation by logging data

--- app/src/main/java/com/oganbelema/triviaapp/MainActivity.kt
+++ app/src/main/java/com/oganbelema/triviaapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.oganbelema.triviaapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.oganbelema.triviaapp.ui.theme.TriviaAppTheme
+import com.oganbelema.triviaapp.viewmodel.QuestionViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,9 +26,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    val viewModel by viewModels<QuestionViewModel>()
+                    TriviaHome(viewModel = viewModel)
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun TriviaHome(viewModel: QuestionViewModel) {
+    Questions(viewModel)
+}
+
+@Composable
+fun Questions(viewModel: QuestionViewModel){
+    val questions = viewModel.dataOrException.value.data
+
+    if (viewModel.dataOrException.value.isLoading == true) {
+        Log.d("Loading", "Loading.....")
+    } else {
+        Log.d("Loading", "Loading Stopped.....")
+
+        questions?.get(0).let { question ->
+            Log.d("Result", "Question: ${question?.question}")
         }
     }
 }
