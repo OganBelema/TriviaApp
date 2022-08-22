167c2d150df54c9f858abdd935cb02ef0e471f04
Oganbelema <oganbelema@gmail.com>
2022-08-22 17:51:17 +0100

    added composable component to display question and choices that react to correct answer selected

--- app/src/main/java/com/oganbelema/triviaapp/component/Questions.kt
+++ app/src/main/java/com/oganbelema/triviaapp/component/Questions.kt
@@ -2,14 +2,21 @@ package com.oganbelema.triviaapp.component
 
 import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
@@ -20,6 +27,7 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.oganbelema.triviaapp.model.Question
 import com.oganbelema.triviaapp.util.AppColors
 import com.oganbelema.triviaapp.viewmodel.QuestionViewModel
 
@@ -36,25 +44,95 @@ fun Questions(modifier: Modifier = Modifier,
     } else {
         Log.d("Loading", "Loading Stopped.....")
 
-        questions?.get(0).let { question ->
-            Log.d("Result", "Question: ${question?.question}")
+        questions?.get(0)?.let { question ->
+            QuestionDisplay(question = question)
         }
     }
 }
 
-@Preview
+
 @Composable
-fun QuestionDisplay() {
+fun QuestionDisplay(
+    question: Question,
+    //questionIndex: MutableState<Int>,
+    //viewModel: QuestionViewModel,
+    onNextClicked: (Int) -> Unit = {}
+) {
+    val choicesState = remember(question) {
+        question.choices.toMutableList()
+    }
+
+    val answerState = remember(question) {
+        mutableStateOf<Int?>(null)
+    }
+
+    val correctAnswerState = remember(question) {
+        mutableStateOf<Boolean?>(null)
+    }
+
+    val updateAnswer: (Int) -> Unit = remember(question) {
+        {
+            answerState.value = it
+            correctAnswerState.value = choicesState[it] == question.answer
+        }
+    }
+
     Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()
+        .fillMaxSize()
         .padding(4.dp),
     color = AppColors.darkPurple) {
         Column(modifier = Modifier.padding(12.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start) {
             QuestionTracker()
-            DrawDottedLine(pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f))
+            DrawDottedLine(
+                pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f))
+            
+            Column {
+                Text(text = question.question,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .align(alignment = Alignment.Start)
+                        .fillMaxHeight(0.3f),
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp,
+                    color = AppColors.offWhite)
+
+                //choices
+                choicesState.forEachIndexed { index, answerText ->
+                    Row(modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxWidth()
+                        .height(45.dp)
+                        .border(
+                            width = 4.dp,
+                            brush = Brush.linearGradient(
+                                colors = listOf(
+                                    AppColors.offDarkPurple,
+                                    AppColors.offDarkPurple
+                                )
+                            ), shape = RoundedCornerShape(15.dp)
+                        )
+                        .clip(RoundedCornerShape(50))
+                        .background(Color.Transparent),
+                    verticalAlignment = Alignment.CenterVertically) {
+
+                        RadioButton(selected = (answerState.value == index), onClick = {
+                            updateAnswer(index)
+                        }, modifier = Modifier.padding(start = 16.dp),
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor =
+                            if (correctAnswerState.value == true && index == answerState.value)
+                                Color.Green.copy(alpha = 0.2f)
+                            else
+                                Color.Red.copy(alpha = 0.2f)
+                        ))
+                        
+                        Text(text = answerText)
+                    }
+                }
+            }
         }
     }
 }
