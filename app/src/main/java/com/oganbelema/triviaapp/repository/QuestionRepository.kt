08fcb872b52138384ebdfb79c42e475cd235c761
Oganbelema <oganbelema@gmail.com>
2022-08-22 11:25:00 +0100

    added repository for question data

--- app/src/main/java/com/oganbelema/triviaapp/repository/QuestionRepository.kt
+++ app/src/main/java/com/oganbelema/triviaapp/repository/QuestionRepository.kt
@@ -1,4 +1,24 @@
 package com.oganbelema.triviaapp.repository
 
-class QuestionRepository {
+import com.oganbelema.triviaapp.data.DataOrException
+import com.oganbelema.triviaapp.model.Trivia
+import com.oganbelema.triviaapp.network.QuestionApi
+import javax.inject.Inject
+
+class QuestionRepository @Inject constructor(private val questionApi: QuestionApi) {
+
+    private val dataOrException = DataOrException<List<Trivia>, Boolean, Exception>()
+
+    suspend fun getMusicQuestions(): DataOrException<List<Trivia>, Boolean, Exception> {
+        try {
+            dataOrException.isLoading = true
+            dataOrException.data = questionApi.getMusicQuestions()
+            if (dataOrException.data != null) dataOrException.isLoading = false
+        } catch (exception: Exception) {
+            dataOrException.error = exception
+            exception.printStackTrace()
+        }
+
+        return dataOrException
+    }
 }
