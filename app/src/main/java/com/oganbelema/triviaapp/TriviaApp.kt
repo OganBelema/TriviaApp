97642915c444e8309c4d7f49dc2a623777d6180c
Oganbelema <oganbelema@gmail.com>
2022-08-22 10:39:20 +0100

    added internet permission, created trivia app and setup hilt

--- app/src/main/java/com/oganbelema/triviaapp/TriviaApp.kt
+++ app/src/main/java/com/oganbelema/triviaapp/TriviaApp.kt
@@ -1,4 +1,7 @@
 package com.oganbelema.triviaapp
 
-class TriviaApp {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TriviaApp: Application() {}
