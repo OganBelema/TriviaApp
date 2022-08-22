c2a132ac7dc57c8a0eafcabf3e4f12d9c5ae8343
Oganbelema <oganbelema@gmail.com>
2022-08-22 10:40:28 +0100

    added question api service and provided retrofit and question api service in app module

--- app/src/main/java/com/oganbelema/triviaapp/di/AppModule.kt
+++ app/src/main/java/com/oganbelema/triviaapp/di/AppModule.kt
@@ -1,4 +1,31 @@
 package com.oganbelema.triviaapp.di
 
+import com.oganbelema.triviaapp.repository.QuestionApi
+import com.oganbelema.triviaapp.util.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideQuestionApi(retrofit: Retrofit): QuestionApi {
+        return retrofit.create(QuestionApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
--- app/src/main/java/com/oganbelema/triviaapp/repository/QuestionApi.kt
+++ app/src/main/java/com/oganbelema/triviaapp/repository/QuestionApi.kt
@@ -1,4 +1,11 @@
 package com.oganbelema.triviaapp.repository
 
+import com.oganbelema.triviaapp.model.Trivia
+import retrofit2.http.GET
+import javax.inject.Singleton
+
+@Singleton
 interface QuestionApi {
+    @GET("music.json")
+    suspend fun getMusicQuestions(): List<Trivia>
 }
